d482b043aca4dfe0967ec28f2b107ab298f3ceee
Sarthak Vijay <1ms19ch036@gmail.com>
2026-08-21 15:58:00 +0530

    Something New

---
+++ Server/server1.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 3003;
+app.use(express.json());
+let students = [
+    {
+        id : 1,
+        name :"Sarthak",
+        course : "MCA",
+        age :22
+    },
+    {
+         id : 2,
+        name :"Vijay",
+        course : "BE",
+        age :22
+    },
+    {
+         id : 3,
+        name :"Himmuuu",
+        course : "MCA",
+        age :22
+    }
+];
+app.get('/', (req, res) => {
+    res.json({
+        message: "Welcome to student Rest API"
+    });
+});
+
+app.get("/api/student", (req, res) =>{
+    res.status(200).json(students);
+});
+app.listen(port, () =>{
+    console.log(`Rest API running at http://localhost:${port}`);
+});
