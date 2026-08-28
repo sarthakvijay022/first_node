c522b06b74b9b643911759c1c395ef58e2bd9cd1
Sarthak Vijay <1ms19ch036@gmail.com>
2026-08-28 15:59:18 +0530

    Some changes in file

--- Server/server1.js
+++ Server/server1.js
@@ -31,6 +31,40 @@ app.get('/', (req, res) => {
 app.get("/api/student", (req, res) =>{
     res.status(200).json(students);
 });
+
+app.get("/api/students/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const student = students.find(student => student.id === id);
+
+    if(!student){
+        return res.status(404).json({
+            message: "Student not found"
+        });
+    }
+    res.status(200).json(student);
+});
+
+app.post("/api/students", (req, res) => {
+    const {name, course, age} = req.body;
+
+    if(!name || !course || !age){
+        return res.status(400).json({
+            message: "Name, Course and age are required"
+        });
+    }
+    const newStudent = {
+        id: students.length+1,
+        name,
+        course,
+        age
+    };
+
+    students.push(newStudent);
+
+    res.status(201).json(newStudent);
+});
+
 app.listen(port, () =>{
     console.log(`Rest API running at http://localhost:${port}`);
 });
